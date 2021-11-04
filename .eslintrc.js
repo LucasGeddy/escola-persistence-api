1ce89aef4a51b239df4cdd1be63d407e6603c8a5
Lucas Sussermann <Sussermann@gmail.com>
2021-11-04 00:42:26 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 'off',
+        indent: ['error', 4],
+        'class-methods-use-this': 'off',
+    },
+};
---
+++ server.js
@@ -0,0 +1,4 @@
+import app from './app';
+
+console.log('App started on http://localhost:4044');
+app.listen(4044);
