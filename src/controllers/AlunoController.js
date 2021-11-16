e3eee9831900ada1b6c7b080be26461793b6825e
Lucas Sussermann <Sussermann@gmail.com>
2021-11-16 16:04:09 -0300

    Students CRUD completed

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,16 +1,79 @@
 import Aluno from '../models/Aluno';
 
-class HomeController {
+class AlunoController {
     async create(req, res) {
         try {
             const novoAluno = await Aluno.create(req.body);
-            res.json(novoAluno);
+            return res.json(novoAluno);
         } catch (e) {
-            res.status(400).json({
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async listAll(req, res) {
+        try {
+            const alunos = await Aluno.findAll();
+            return res.json(alunos);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async show(req, res) {
+        try {
+            if (!req.params.id) return res.status(401).json({ errors: 'Invalid Aluno.id' });
+
+            const aluno = await Aluno.findByPk(req.params.id);
+
+            if (!aluno) return res.status(404).json({ errors: 'Aluno not found' });
+
+            return res.json(aluno);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async update(req, res) {
+        try {
+            if (!req.params.id) return res.status(401).json({ errors: 'Invalid Aluno.id' });
+
+            const aluno = await Aluno.findByPk(req.params.id);
+
+            if (!aluno) return res.status(404).json({ errors: 'Aluno not found' });
+
+            const alunoAtualizado = await aluno.update(req.body);
+
+            return res.json(alunoAtualizado);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map((err) => err.message),
+            });
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            if (!req.params.id) return res.status(401).json({ errors: 'Invalid Aluno.id' });
+
+            const aluno = await Aluno.findByPk(req.params.id);
+
+            if (!aluno) return res.status(404).json({ errors: 'Aluno not found' });
+
+            await aluno.destroy();
+
+            return res.json(aluno);
+        } catch (e) {
+            return res.status(400).json({
                 errors: e.errors.map((err) => err.message),
             });
         }
     }
 }
 
-export default new HomeController();
+export default new AlunoController();
